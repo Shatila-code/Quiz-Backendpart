b4f3f1d94e974142bd295a7b91176d77fd46ebc3
Abedulwahab <a210072@students.gu.edu.lb>
2025-04-27 18:23:58 +0300

    added a function to check if the hashed password matches the user's pass stored in the database

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+include 'connection.php';
+
+if($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+
+     if(!isset($_POST['email'],$_POST['password'])){
+        http_response_code(400);
+        echo json_encode(['error'=>'Email and password are required .']);
+        exit;
+     }
+}
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+
+try{
+    $query = $pdo->prepare(" SELECT * FROM users WHERE Email = :email");
+     $query->bindParam(":email",$email,PDO::PARAM_STR);
+     $query->execute();
+ 
+
+     if($user && password_verify($password, $user['Password'])){
+
+        unset($user['Password']);
+
+        echo json_encode([
+            'message'=> ' successfull Login :)',
+            'user'=>$user
+        ]);
+     }
+}
+
+
+catch(){
+
+}
